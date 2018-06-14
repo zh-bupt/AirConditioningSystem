3db8308ef65629699d92f773535b441f0fd57b9f
zh-bupt <zh2015211203@bupt.edu.cn>
2018-06-14 21:22:18 +0800

    完善了报表类,可能还需要修改

--- server/src/server/MonthlyReporter.java
+++
@@ -1,7 +0,0 @@
-package server;
-
-import java.util.List;
-
-public class MonthlyReporter extends Reporter {
-
-}
--- server/src/server/WeeklyReporter.java
+++
@@ -1,7 +0,0 @@
-package server;
-
-import java.util.List;
-
-public class WeeklyReporter extends Reporter{
-
-}
