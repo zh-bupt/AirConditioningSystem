b5a6f14ab6884cde6940397b32ca0f69ec9cec0e
great15 <1164895624@qq.com>
2018-06-19 09:00:33 +0800

    check增加账单信息显示

--- server/src/server/controller/UI_CustomerCheck_Controller.java
+++ server/src/server/controller/UI_CustomerCheck_Controller.java
@@ -1,6 +1,8 @@
 package server.controller;
 
+import server.manager.BillManager;
 import server.manager.CustomerManager;
+import server.simpleclass.Bill;
 import server.simpleclass.Customer;
 import server.ui.UI_CustomerCheck;
 import server.ui.UI_Register;
@@ -17,6 +19,7 @@ public class UI_CustomerCheck_Controller {
     private List<String> checkingRooms = new ArrayList<>();
     private String room_id;
     private Map<Socket, String> customerMap = null;
+    private Bill bill;
 
     private void getData(){
         checkingRooms.clear();
@@ -39,15 +42,19 @@ public class UI_CustomerCheck_Controller {
             checkingRooms.add("Room4");
         }
 
+
     }
     class Button1Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             room_id = ui_customerCheck.getRoomID();
+            bill = BillManager.getInstance().getBill(room_id);
+            ui_customerCheck.setTextArea("房间号："+bill.getRoomId()+"\n电量："+bill.getElectricity()+"\n费用："+bill.getCost()+"\n创建时间:"+bill.getCreateTime());
             if(CustomerManager.getInstance().check(room_id))
             {
-                JOptionPane.showMessageDialog(ui_customerCheck,"结账离开成功！"+"房间号："+room_id);
+                JOptionPane.showMessageDialog(ui_customerCheck,"结账离开成功！账单信息请看下方文本框！"+"房间号："+room_id);
                 getData();
                 ui_customerCheck.setCheckingRooms(checkingRooms);
+
                 ui_customerCheck.refreshUI();
             }
             else
@@ -57,6 +64,9 @@ public class UI_CustomerCheck_Controller {
     public UI_CustomerCheck_Controller(UI_CustomerCheck customerCheck) {
         ui_customerCheck = customerCheck;
         getData();
+//        bill = BillManager.getInstance().getBill("1001");
+//        ui_customerCheck.setTextArea("房间号："+bill.getRoomId()+"\n电量："+bill.getElectricity()+"\n费用："+bill.getCost()+"\n创建时间:"+bill.getCreateTime());
+
         ui_customerCheck.setCheckingRooms(checkingRooms);
         ui_customerCheck.refreshUI();
         ui_customerCheck.addButton1Listener(new Button1Listener());
--- server/src/server/ui/UI_CustomerCheck.java
+++ server/src/server/ui/UI_CustomerCheck.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 import java.util.*;
 
 public class UI_CustomerCheck extends JFrame {
-    private JTextField textField2;
+    private JTextArea textArea = new JTextArea();
     private JComboBox comboBox1;
     private JLabel l1,l2;
     private JButton button1;
@@ -15,7 +15,7 @@ public class UI_CustomerCheck extends JFrame {
     public UI_CustomerCheck(){
         init();
         setVisible(true);
-        setSize(300,200);
+        setSize(300,300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
     }
@@ -26,24 +26,23 @@ public class UI_CustomerCheck extends JFrame {
         this.setTitle("结账离开");
 
         l1 = new JLabel("房间号:");
-        l1.setBounds(50,30,80,30);
+        l1.setBounds(50,10,80,30);
+        l2 = new JLabel("账单:");
+        l2.setBounds(50,40,80,30);
         checkingRooms.add("India");
         checkingRooms.add("Aus");
         checkingRooms.add("U.S.A");
         comboBox1=new JComboBox(checkingRooms.toArray());
-        comboBox1.setBounds(110, 30,160,30);
+        comboBox1.setBounds(110, 10,160,30);
+        textArea.setBounds(50,70,200,150);
 
-
-//        l2 = new JLabel("身份证号:");
-//        l2.setBounds(50,80,80,30);
-//        textField2 = new JTextField("5");
-//        textField2.setBounds(110,80,160,30);
 
 
         this.add(l1);this.add(comboBox1);
-//        this.add(l2);this.add(textField2);
+        this.add(l2);
+        this.add(textArea);
         button1 = new JButton("结账离开");
-        button1.setBounds(100,130,80,30);
+        button1.setBounds(100,230,80,30);
         this.add(button1);
 
         this.setLayout(null);
@@ -57,7 +56,7 @@ public class UI_CustomerCheck extends JFrame {
     public void refreshUI(){
         remove(comboBox1);
         comboBox1=new JComboBox(checkingRooms.toArray());
-        comboBox1.setBounds(110, 30,160,30);
+        comboBox1.setBounds(110, 10,160,30);
         this.add(comboBox1);
 
         revalidate();
@@ -68,9 +67,10 @@ public class UI_CustomerCheck extends JFrame {
         return comboBox1.getItemAt(comboBox1.getSelectedIndex()).toString();
     }
 
-//    public String getUserID(){
-//        return textField2.getText();
-//    }
+    public void setTextArea(String i){
+        textArea.setText(i);
+    }
+
     public void addButton1Listener(ActionListener mal) {
         button1.addActionListener(mal);
     }
